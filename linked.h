ac2f18c6cbfa7797d6c7096bd02ea403ae7fa05c
ARES <132830889+Yoares@users.noreply.github.com>
2025-02-15 16:12:32 +0100

    yeey

--- linked.h
+++ linked.h
@@ -13,6 +13,5 @@ typedef struct t_list{
 void addfront(t_list **list, t_list *new_node);
 void addback(t_list **list, t_list *node);
 t_list *ft_lstnew(int node);
-void init_stack(t_list *stack);
 void push_stack(t_list **stack_a, t_list **stack_b);
 #endif
