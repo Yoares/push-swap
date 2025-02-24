69aed15d76ac94ce44c67bf4254104d7e61468b9
ARES <132830889+Yoares@users.noreply.github.com>
2025-02-24 16:44:31 +0000

    Update range_sort.c

--- range_sort.c
+++ range_sort.c
@@ -1,12 +1,70 @@
 #include "push_swap.h"
 
-int get_range(int size)
+int	set_array(t_stack *stack_a)
 {
-	if (size <= 100)
-		return (size / 6);
-	else if (size <= 500)
-		return (size / 14);
-	else
-		return (size / 20);
+	int	i;
+	int *arr;
+	i = 0;
+	while (stack_a)
+	{
+		arr[i++] = stack_a->data;
+		stack_a = stack_a->next;
+	}
 }
 
+void	swap(int *x, int *y)
+{
+	int *tmp;
+
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+void	sort_arr(int *arr, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		j = 0;
+		while (j < size - i - 1)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				swap(&arr[j], &arr[j + 1]);
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	get_index(t_stack **stack)
+{
+	int *arr;
+	int	i;
+	int size;
+	t_stack *tmp;
+
+	size = lstsize(*stack);
+	arr = set_array(stack);
+	tmp = *stack;
+
+	while (tmp)
+	{
+		i = 0;
+		while (i < size)
+		{
+			if (arr[i] == tmp->data)
+			{
+				tmp->index = i + 1;
+				break;
+			}
+			i++;
+		}
+		tmp = tmp->next;
+	}
+}
