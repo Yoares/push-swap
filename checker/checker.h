edafc78016644cf0ccc5f43331d62f795cbbb14d
Yasser Khoussi <ykhoussi@e1r4p9.1337.ma>
2025-03-04 01:23:27 +0000

    pushy

---
+++ checker/checker.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykhoussi <ykhoussi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 00:40:09 by ykhoussi          #+#    #+#             */
+/*   Updated: 2025/03/04 00:43:03 by ykhoussi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHECKER_H
+# define CHECKER_H
+
+# include "./../Libft/libft.h"
+# include "./../push_s/push.h"
+# include <stdint.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+
+#endif
